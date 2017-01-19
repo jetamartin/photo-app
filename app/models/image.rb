9d92d1ecede4d50a83762906fff4990e884acdad
jetamartin <jetamartin@yahoo.com>
2017-01-19 18:23:31 +0000

    fix problem with picture_size method in Image.rb

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,13 +1,14 @@
 class Image < ActiveRecord::Base
   belongs_to :user
   # Using Carrierwave to associate image to Image model and mount_uploader is the method to use
+  validate :picture_size
   mount_uploader :picture, PictureUploader
   # validate :picture_size
   
-  # private
-  # def picture_size
-  #   if picture_size > 5.megabytes
-  #     errors.add(:picture, "should be less than 5MB")
-  #   end
-  # end
+  private
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,7 +1,7 @@
 class PictureUploader < CarrierWave::Uploader::Base
   
   include CarrierWave::MiniMagick
-  process resize_to_limit: [300, 300]
+  process resize_to_limit: [200, 200]
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
